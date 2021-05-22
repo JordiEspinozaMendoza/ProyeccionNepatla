1fe2fe9e7435ccd8b094cc9327a8713e7e27c053
Jordi <jordi8101@gmail.com>
2021-05-22 13:28:00 -0700

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v^#^da@csgj(8t^jt&v6uxkl_bkj-bc4o4hg296r3mtq238qib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','proyeccionnepatla.herokuapp.com']
 
 
 # Application definition
